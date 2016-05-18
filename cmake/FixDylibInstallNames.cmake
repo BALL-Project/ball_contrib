d12023b0c4f498ac7fe023d056015c75671bbdb4
pthiel <thiel@informatik.uni-tuebingen.de>
2016-05-18 11:03:31 +0200

    Fix also names of dependent shared libraries on OSX

--- cmake/FixDylibInstallNames.cmake
+++ cmake/FixDylibInstallNames.cmake
@@ -6,11 +6,30 @@ FILE(GLOB_RECURSE dylibs "${LIBNAME}*.dylib")
 
 
 FOREACH(dylib ${dylibs})
+	GET_FILENAME_COMPONENT(base_name ${dylib} NAME)
 
 	# Only process the library itself and not symlinks to it
 	IF(NOT IS_SYMLINK ${dylib})
-		# Call install_name_tool to set the install name appropriately
-		EXECUTE_PROCESS(COMMAND install_name_tool -id ${dylib} ${dylib})
+
+		EXECUTE_PROCESS(COMMAND otool -L ${dylib} OUTPUT_VARIABLE temp)
+		SEPARATE_ARGUMENTS(deps UNIX_COMMAND ${temp})
+		LIST(REMOVE_AT deps 0)
+
+		FOREACH(dep ${deps})
+			IF(${dep} MATCHES ".?dylib" AND NOT IS_ABSOLUTE ${dep})
+				GET_FILENAME_COMPONENT(name ${dep} NAME)
+				GET_FILENAME_COMPONENT(abs_name ${dep} ABSOLUTE)
+
+				IF(${name} STREQUAL ${base_name})
+					# Set asolute path of dylib as its install name
+					EXECUTE_PROCESS(COMMAND install_name_tool -id "${abs_name}" ${dylib})
+				ELSE()
+					# Set absolute paths of dependent shared libraries
+					EXECUTE_PROCESS(COMMAND install_name_tool -change "${name}" "${abs_name}" ${dylib})
+				ENDIF()
+			ENDIF()
+		ENDFOREACH()
 	ENDIF()
 
 ENDFOREACH()
+
