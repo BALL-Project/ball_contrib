27aaf30f27b09741e467b90be937695d5cbfe856
pthiel <thiel@informatik.uni-tuebingen.de>
2016-11-04 14:02:19 +0100

    Moved download step to CMake run
    This enables us to spike in packages manually for development purposes.
    Furthermore, it can be used to generate contrib tarballs without need
    for downloading anything.

--- cmake/ContribMacros.cmake
+++ cmake/ContribMacros.cmake
@@ -32,9 +32,6 @@
 # -----------------------------------------------------------------------------
 
 
-###############################################################################
-# Macros
-
 # Create a list of all packages available in this contrib and a list of packages selected for installation
 MACRO(EVALUATE_PACKAGES)
 
@@ -59,11 +56,52 @@ MACRO(EVALUATE_PACKAGES)
 ENDMACRO()
 
 
+# Configure and download (git clone) packages
+MACRO(CONFIGURE_PACKAGES)
+
+	FOREACH(PACKAGE ${PACKAGES_SELECTED})
+
+		# Check if selected package is indeed an available one
+		LIST(FIND PACKAGES_AVAILABLE ${PACKAGE} PACKAGE_FOUND)
+
+		IF(${PACKAGE_FOUND} EQUAL -1)
+			MESSAGE(SEND_ERROR "The selected package '${PACKAGE}' is not part of BALL_contrib.")
+		ELSE()
+			# Configure package
+			MESSAGE(STATUS "Configuring project: ${PACKAGE}")
+			INCLUDE("${CONTRIB_LIBRARY_PATH}/${PACKAGE}.cmake")
+
+			# Check if package has already been cloned, if not, clone it
+			IF(NOT pkg_${PACKAGE}_downloaded)
+				MESSAGE(STATUS "Downloading package (git clone): ${PACKAGE}")
+				EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE} clone --branch "${CONTRIB_GIT_BRANCH}" "${CONTRIB_GITHUB_BASE}/${pkg_${PACKAGE}}" "${CONTRIB_BINARY_SRC}/${PACKAGE}"
+						TIMEOUT 100
+						RESULT_VARIABLE DOWNLOAD_EXIT_CODE
+						OUTPUT_FILE "${CONTRIB_BINARY_SRC}/${PACKAGE}_download_out.log"
+						ERROR_FILE "${CONTRIB_BINARY_SRC}/${PACKAGE}_download_err.log"
+						)
+
+				# Check if download was successful
+				IF(${DOWNLOAD_EXIT_CODE} EQUAL 0)
+					# If yes, mark package as donwloaded in CMake internal cache
+					SET(pkg_${PACKAGE}_downloaded TRUE CACHE INTERNAL "Variable indicates that package ${PACKAGE} has already been cloned")
+				ELSE()
+					# If not, exit CMake run with fatal error
+					MESSAGE(FATAL_ERROR "Downloading of package failed: ${PACKAGE}")
+				ENDIF()
+			ENDIF()
+		ENDIF()
+
+	ENDFOREACH()
+
+ENDMACRO()
+
+
 # Macro to write absolute paths as install names for dylibs using install_name_tool
 MACRO(FIX_DYLIB_INSTALL_NAMES DYLIB_PREFIX)
 
-        # As this procedure has to wait until the libraries are built and installed,
-        # fixing the names is added as an ExternalProject step depending on the install step.
+	# As this procedure has to wait until the libraries are built and installed,
+	# fixing the names is added as an ExternalProject step depending on the install step.
 	ExternalProject_Add_Step(${PACKAGE} fixnames
 
 		LOG 1
@@ -75,15 +113,3 @@ MACRO(FIX_DYLIB_INSTALL_NAMES DYLIB_PREFIX)
 
 ENDMACRO()
 
-
-###############################################################################
-# Messages
-
-MACRO(MSG_CONFIGURE_PACKAGE_BEGIN package_name)
-	MESSAGE(STATUS "Configuring project: ${package_name}")
-ENDMACRO()
-
-
-MACRO(MSG_CONFIGURE_PACKAGE_END package_name)
-	MESSAGE(STATUS "Configuring ${package_name} - done")
-ENDMACRO()
